79bc69efafde1f904fe0bc5673162c1a28549a7c
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-03 11:12:21 -0300

    ex025

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,3 +1,6 @@
+num = int(input('Digite um número entre 0 e 9999: '))
 
-num = int('Digite um número entre 0 e 9999: ')
-
+print('Unidade: {}'.format(num%10))
+print('Dezena: {}'.format((num//10)%10))
+print('Centena: {}'.format((num//100)%10))
+print('Milhar: {}'.format(num//1000))
---
+++ exercicios/ex024.py
@@ -0,0 +1,3 @@
+cidade = input('Digite a cidade em que você nasceu: ')
+cidadeinc = cidade.split()[0]
+print('Santo' in cidadeinc)
