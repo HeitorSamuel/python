13770bddd66d0b7d67902b623f1d1e8ff4ef2df7
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-05 16:09:29 -0300

    ex028 corrigido

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,8 +1,14 @@
 from random import randint
-numAdv = int(input('Adivinhe o número que estou pensando entre 0 e 5: '))
+from time import sleep
 numPens = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5...')
+print('-=-' * 20)
+numAdv = int(input('Adivinhe o número que estou pensando entre 0 e 5: '))
+print('PROCESSANDO...')
+sleep(3)
 if numAdv == numPens:
     print('Você venceu! Eu pensei no n° {} e você no n° {}!'.format(numPens, numAdv))
 else:
-    print('Você perdeu! Eu pensei no n° {} e você no n° {}'.format(numPens, numAdv))
+    print('Eu ganhei! Eu pensei no n° {} e você no n° {}'.format(numPens, numAdv))
 print('-'*10 + 'FIM' + '-'*10)
