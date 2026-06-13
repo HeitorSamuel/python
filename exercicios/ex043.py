ef839ff5a09efc98734058b008688c1aae7bee88
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-13 10:38:39 -0300

    ex 044

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -3,12 +3,12 @@ altura = float(input('Digite a sua altura em metros(m): '))
 imc = peso/(altura**2)
 
 if imc < 18.5:
-    print('O seu imc é {:.2f}! Você está ABAIXO DO PESO!'.format(imc))
-elif imc >= 18.5 and imc <= 25:
-    print('O seu imc é {:.2f}! Você está no PESO IDEAL!'.format(imc))
-elif imc > 25 and imc <= 30:
-    print('O seu imc é {:.2f}! Você está com SOBREPESO!'.format(imc))
-elif imc > 30 and imc <= 40:
-    print('O seu imc é {:.2f}! Você está com OBESIDADE'.format(imc))
-elif imc > 40:
-    print('O seu imc é {:.2f}! Você está COM OBESIDADE MÓRBIDA!'.format(imc))
+    print('O seu IMC é {:.1f}! Você está ABAIXO DO PESO!'.format(imc))
+elif imc >= 18.5 and imc < 25:
+    print('O seu IMC é {:.1f}! Você está no PESO IDEAL!'.format(imc))
+elif imc >= 25 and imc < 30:
+    print('O seu IMC é {:.1f}! Você está com SOBREPESO!'.format(imc))
+elif imc >= 30 and imc < 40:
+    print('O seu IMC é {:.1f}! Você está com OBESIDADE'.format(imc))
+elif imc >= 40:
+    print('O seu IMC é {:.1f}! Você está COM OBESIDADE MÓRBIDA!'.format(imc))
--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -1,10 +1,10 @@
-print('='*11 + ' LOJAS SAMUEL ' + '='*11)
+print('{:=^40}'.format(' LOJAS SAMUEL '))
 precoComp = int(input('Digite o preço das compras: R$'))
 print('FORMAS DE PAGAMENTO')
 print('[ 1 ] à vista no dinheiro/cheque')
 print('[ 2 ] à vista no cartão')
 print('[ 3 ] 2x no cartão')
-print('[ 4 ] 3x no cartão')
+print('[ 4 ] 3x ou mais no cartão')
 opcPag = int(input('Qual opção você prefere? '))
 
 if opcPag == 1:
@@ -12,6 +12,12 @@ if opcPag == 1:
 elif opcPag == 2:
     print('O valor da sua compra de R${:.2f} vai passar a custar R${:.2f}!'.format(precoComp, precoComp*0.95))
 elif opcPag == 3:
-    print('O valor da sua compra de R${:.2f} vai continuar com o mesmo valor!'.format(precoComp))
+    print('Sua compra será parcelada em 2x de R${:.2f} SEM JUROS. \nO valor da sua compra de R${:.2f} vai continuar com o mesmo valor!'.format(precoComp/2,  precoComp))
 elif opcPag == 4:
-    print('O valor da sua compra de R${:.2f} vai passar a custar R${:.2f}!'.format(precoComp, precoComp*1.2))
+    parcelas = int(input('Quantas parcelas? '))
+    if parcelas < 3:
+        print('Digite um valor válido! Tente novamente!')
+    else:
+        print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS \nO valor da sua compra de R${:.2f} vai passar a custar R${:.2f}!'.format((parcelas),(precoComp*1.2/parcelas), (precoComp), (precoComp*1.2)))
+else:
+    print('Opção INVÁLIDA de pagamento! Tente Novamente!')
