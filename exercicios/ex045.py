d5419718307cbb64abb86626a021c69b0f73cd7c
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-13 14:44:54 -0300

    CORRGINDO EX 045

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,18 +1,28 @@
 from random import choice
+from time import sleep
 
 print('='*11 + ' JOGANDO JOKENPÔ ' + '='*11)
 
-usuario = str(input('Escolha entre Pedra, Papel ou Tesoura: ')).strip().lower()
+usuario = str(input('Escolha entre Pedra, Papel ou Tesoura: ')).strip().upper()
+print('-='*22)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!!!')
+print('-='*22)
 
 comput = ['pedra', 'papel', 'tesoura']
 
-sorteio = choice(comput)
+sorteio = choice(comput).upper()
 
-if (usuario == 'pedra' and sorteio == 'pedra') or (usuario == 'papel' and sorteio == 'papel') or (usuario == 'tesoura' and sorteio == 'tesoura'):
+if (usuario == 'PEDRA' and sorteio == 'PEDRA') or (usuario == 'PAPEL' and sorteio == 'PAPEL') or (usuario == 'TESOURA' and sorteio == 'TESOURA'):
     print('EMPATE! Eu escolhi: {} e você escolheu: {}'.format(sorteio, usuario))
-elif (usuario == 'pedra' and sorteio == 'tesoura') or (usuario == 'papel' and sorteio == 'pedra') or (usuario == 'tesoura' and sorteio == 'papel'):
+elif (usuario == 'PEDRA' and sorteio == 'TESOURA') or (usuario == 'PAPEL' and sorteio == 'PEDRA') or (usuario == 'TESOURA' and sorteio == 'PAPEL'):
     print('VOCÊ VENCEU! Você escolheu: {} e eu escolhi: {}'.format(usuario, sorteio))
-elif (sorteio == 'pedra' and usuario == 'tesoura') or (sorteio == 'papel' and usuario == 'pedra') or (sorteio == 'tesoura' and usuario == 'papel'):
+elif (sorteio == 'PEDRA' and usuario == 'TESOURA') or (sorteio == 'PAPEL' and usuario == 'PEDRA') or (sorteio == 'TESOURA' and usuario == 'PAPEL'):
     print('EU VENCI! Eu escolhi: {} e você escolheu: {}'.format(sorteio, usuario))
+else:
+    print('Carater Inválido! Tente Novamente!')
 
 print('='*17 + ' FIM ' + '='*17)
