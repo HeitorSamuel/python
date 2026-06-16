18b95c534985ed98eea5feaae7b18fbc98775836
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-16 11:15:08 -0300

    fazendoex052

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,1 +1,10 @@
 frase = str(input('Digite qualquer frase: ')).strip()
+fraseComprim = len(frase)-1
+
+for i in range(fraseComprim, -1, -1):
+    (frase[i])
+        
+
+'''print('Esta frase é um PALÍNDROMO! As frases {} e {} são iguais!'.format(frase, frase[c]))
+    else:
+        print('Esta frase não é um PALÍNDROMO! As frases {} e {} são diferentes!'.format(frase, frase[c]))'''
