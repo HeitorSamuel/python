34856f4bd8396580b32215b2334dbf450f2831d9
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-17 11:02:25 -0300

    ex054 sendo feito

--- exercicios/ex052.py
+++ exercicios/ex052.py
@@ -1,5 +1,14 @@
 num = int(input('Digite um número inteiro: '))
+contDivisores = 0
+
 if num <= 1:
-    print('O número {} não é um número primo!')
+    print('O número {} não é um número primo!'.format(num))
 else:
-    if num 
+    for c in range(1, (num+1)):
+        if num % c == 0:
+            contDivisores += 1
+    if contDivisores == 2:
+        print('O número {} é um número PRIMO! Ele possui {} divisores!'.format(num, contDivisores))
+    elif contDivisores > 2:
+        print('O número {} é um número COMPOSTO! Ele tem {} divisores!'.format(num, contDivisores))
+
