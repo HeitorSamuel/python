c91a04914d8b5f2b9a358b576546cc7a5d4c0260
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-19 11:19:29 -0300

    fazendo o ex056

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,4 +1,7 @@
 somaIdade = 0
+maiorIdade = 0
+QuantMulher = 0
+NomeMaiorIdade = ''
 
 for c in range(1, 5):
     print('-='*20)
@@ -6,8 +9,16 @@ for c in range(1, 5):
     idade = int(input('Digite a sua idade: '))
     sexo = str(input('Digite o seu sexo: ')).strip().lower()
     somaIdade += idade
-    if sexo == 'homem':
-        if 
-
+    if sexo == 'masculino':
+        if idade > maiorIdade:
+            maiorIdade = idade
+            NomeMaiorIdade = nome
+    elif sexo == 'feminino':
+        if idade < 20:
+            QuantMulher += 1
+print('-='*20)
+print('A MÉDIA DE IDADE do grupo é: {:.1f}'.format(somaIdade/4))
+print('O nome do HOMEM MAIS VELHO é: {}. Ele tem {} anos.'.format(NomeMaiorIdade, maiorIdade))
+print('A quantidade de mulheres que têm MENOS DE 20 ANOS é: {}'.format(QuantMulher))
 
 print('-='*20)
