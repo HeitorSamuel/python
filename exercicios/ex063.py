821c70806be0b38d8a8c88d7b6cef240c778b105
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-27 11:17:50 -0300

    fazendo o ex064

--- exercicios/ex063.py
+++ exercicios/ex063.py
@@ -1,1 +1,14 @@
-num = int(input('Digite um número inteiro: '))
+print('='*40)
+print('{:^40}'.format(' SEQUÊNCIA DE FIBONACCI '))
+print('='*40)
+
+termo = int(input('Quantos termos você quer? '))
+a = 0
+b = 1
+
+while termo > 0:
+    print(a, end=' → ')
+    b = b + a
+    a = b - a
+    termo -= 1
+print('FIM')
