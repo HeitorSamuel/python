1e5ba849d727e4cf2eae3b12affe1fa68e13bbcf
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-06-30 11:06:14 -0300

    corrigindo ex058 e 059

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -7,13 +7,18 @@ print('Vou pensar em um número entre 0 e 10...')
 print('-=' * 20)
 numUsuario = int(input('Adivinhe um número que estou pensando entre 0 e 10: '))
 print('PROCESSANDO...')
-sleep(1.5)
+sleep(1)
 contPalp = 1
 
 while numComp != numUsuario:
-    numUsuario = int(input('Você ERROU! Tente Novamente! '))
-    print('PROCESSANDO...')
-    sleep(1.5)
+    if numUsuario < numComp:
+        print('Mais... Tente mais uma vez!')
+        numUsuario = int(input('Digite outro número: '))
+    elif numUsuario > numComp:
+        print('Menos... Tenta mais uma vez!')
+        numUsuario = int(input('Digite outro número: '))
     contPalp += 1
-print('Você ACERTOU! eu pensei no n° {} e você no n° {}!'.format(numComp, numUsuario))
-print('Ao todo foram {} palpites para acertar!'.format(contPalp))
+    print('PROCESSANDO...')
+    sleep(1)
+print('Você ACERTOU! eu pensei no número {} e você no número {}!'.format(numComp, numUsuario))
+print('Ao todo foram {} palpite(s) para acertar!'.format(contPalp))
--- exercicios/ex059.py
+++ exercicios/ex059.py
@@ -5,40 +5,53 @@ num2 = float(input('Digite o 2° número: '))
 opcao = 0
 
 while opcao != 5:
-    print('''========================================         
+    print('''      
 ESCOLHA UMA DAS OPÇÕES ABAIXO:
 [1] SOMAR
 [2] MULTIPLICAR
 [3] MAIOR
 [4] NOVOS NÚMEROS
 [5] SAIR DO PROGRAMA    
-========================================''')
+''')
     opcao = int(input('SUA ESCOLHA: '))
-    
     if opcao == 1:
         sleep(1)
+        print('-='*40)
         print('A soma do número {} com o número {} é {}'.format(num1, num2, num1+num2))
+        print('-='*40)
     elif opcao == 2:
         sleep(1)
+        print('-='*40)
         print('A Multiplicação do número {} com o número {} é {}'.format(num1, num2, num1*num2))
+        print('-='*40)
     elif opcao == 3:
         if num1 > num2:
             sleep(1)
+            print('-='*40)
             print('O número {} é maior que o número {}'.format(num1, num2))
+            print('-='*40)
         elif num2 > num1:
             sleep(1)
+            print('-='*40)
             print('O número {} é maior que o número {}'.format(num2, num1))
+            print('-='*40)
         elif num1 == num2:
             sleep(1)
+            print('-='*40)
             print('Não há nunhum número MAIOR! Ambos são iguais a {}!'.format(num1))
+            print('-='*40)
     elif opcao == 4:
+        print('-='*40)
         num1 = float(input('Digite o 1° número: '))
         num2 = float(input('Digite o 2° número: '))
+        print('-='*40)
     elif opcao < 1 or opcao > 5:
         sleep(1)
+        print('-='*40)
         print('Opção Inválida! Tente Novamente!')
+        print('-='*40)
 print('-='*20)
 print('PROCESANDO...')
 print('-='*20)
-sleep(2)
+sleep(1.5)
 print('FIM DO PROGRAMA!')
