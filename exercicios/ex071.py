5be7a3df6bd2b7f15349060dc431d54bddf5ffd9
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-10 11:16:49 -0300

    fazendo o ex071

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -3,4 +3,11 @@ print(f'{'BANCO DE GOTHAM':^40}')
 print('='*40)
 
 pergValor = int(input('Que valor você quer sacar? R$').strip())
-print(pergValor//50)
+
+while True:
+    
+    if ValorRest < 50:
+        ValorRest = pergValor - 50
+    else:
+        break
+print(ValorRest)
