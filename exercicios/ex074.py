ec56e3e10aa4703e0bacee71089db467652f3589
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-15 11:21:55 -0300

    fazendo o ex074

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,3 +1,5 @@
-from random import random
-numero = random()
-print(numero)
+from random import randint
+
+numSort = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+for c in numSort:
+    print(f'Os valores sorteados foram: ')
