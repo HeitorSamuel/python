859d3e58bddcc424169165f41c451837651447e4
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-16 15:03:45 -0300

    ex077 sendo feito

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,9 +1,11 @@
 listagem = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
 
-
 print('-'* 50)
 print(f'{' LISTAGEM DE PREÇOS ':^50}')
 print('-'* 50)
 
-for item in listagem:
-    print(item, end=' ')
+for item in range(0, len(listagem)):
+    if item % 2 == 0:
+        print(f'{listagem[item]:.<40}', end='R$ ' )
+    else:
+        print(f'{listagem[item]:>6.2f}')
---
+++ exercicios/ex077.py
@@ -0,0 +1,5 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
+for c in palavras:
+    if 'AaEeIiOoUu' in c:
+        print()
