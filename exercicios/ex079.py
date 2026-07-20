a666bc1c7ac087363397f88afd7cc46314aa0777
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-20 11:10:40 -0300

    fazendo o ex079

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,5 +1,16 @@
 valores = []
 while True:
-    valor = int(input('Digite um valor: '))
-    for 
-    if valor == 
+    valor = int(input('Digite um valor: ').strip())
+    for i, v in enumerate(valores):
+        valores.append(valor)
+        if v == valor:
+            print('Valor duplicado! Não vou adicionar...')
+        else:
+            valores.append(valor)
+            print('Valor adicionado com sucesso!')
+    '''perg = str(input('Quer continuar? [S/N]').strip().upper())
+    while 'S' not in perg and 'N' not in perg:
+        perg = str(input('Quer continuar? [S/N]').strip().upper())
+    if perg == 'N':
+        break'''
+print(f'Você digitou os valores {valores}')
