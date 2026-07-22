356c8341033e07a50a34d6dbb299cdd338faeaae
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-22 11:11:09 -0300

    ex082 sendo feito

--- exercicios/ex080.py
+++ exercicios/ex080.py
@@ -3,10 +3,17 @@ valores = []
 for c in range(0, 5):
     num = int(input('Digite um número: ').strip())
     if c == 0:
-        valores.insert(-1, num)
+        valores.append(num)
         print('Adicionado ao final da lista...')
     else:
-        if num < 
-    valores.insert(c, num)
-    valores.append(num)
-print(valores)
+        if num < valores[0]:
+            valores.insert(0, num)
+            print('Adicionado na posição 0 da lista...')
+        elif num > valores[0] and num < valores[-1]:
+            valores.insert(1, num)
+            print('Adicionado na posição 1 da lista...')
+        elif num > valores[-1]:
+            valores.append(num)
+            print('Adicionado ao final da lista...')
+print('-='* 30)
+print(f"Os valores digitados em ordem foram {valores}")
---
+++ exercicios/ex081.py
@@ -0,0 +1,19 @@
+valores = []
+cont = 0
+while True:
+    num = int(input('Digite um valor: ').strip())
+    cont += 1
+    valores.append(num)
+    perg  = str(input('Quer continuar? [S/N] ').strip().upper()[0])
+    while perg not in "N" and perg not in "S":
+        perg  = str(input('Quer continuar? [S/N] ').strip().upper()[0])
+    if perg == 'N':
+        break
+print('-='*30)
+valores.sort(reverse=True)
+print(f"Você digitou {cont} elementos.")
+print(f"Os valores em ordem decrescente são {valores}")
+if 5 in valores:
+    print("O valor 5 faz parte da  lista!")
+else:
+    print("O valor 5 não foi encontrado na lista!")
