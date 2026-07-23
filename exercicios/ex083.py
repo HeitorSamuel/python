2b71633bdca8f0454a6a9be8c386b9271be31502
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-07-23 11:21:49 -0300

    fazendo o ex083

--- exercicios/ex083.py
+++ exercicios/ex083.py
@@ -1,7 +1,21 @@
-valor = []
+pilha = []
 
 expressao = str(input('Digite uma expressão: ').strip())
-if 
+
+for c in expressao:
+    if c == '(':
+        pilha.append('(')
+    elif c == ')':
+        if not pilha:
+            print('Expressão inválida!')
+        else:
+            pilha.pop()
+
+if '(' in pilha: 
+    print('Expressão inválida!')
+elif not pilha:
+    print('Expressão válida!')
+'''if '(' in expressao:
     print('Expressão válida')
 else:
-    print('Expressão inválida!')
+    print('Expressão inválida!')'''
