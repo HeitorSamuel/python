d079f97c27b4fd79e5f9c7adeea2873c2bd48a79
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-08-01 15:42:23 -0300

    fazendo o ex089

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -2,7 +2,14 @@ alunosGeral = []
 aluno = []
 
 while True:
-    aluno.append(str(input('Nome: ')))
-    aluno.append(float(input('Nota 1: ')))
-    aluno.append(float(input('Nota 2: ')))
-    
+    aluno.append(str(input('Nome: ').strip()))
+    aluno.append(float(input('Nota 1: ').strip()))
+    aluno.append(float(input('Nota 2: ').strip()))
+    alunosGeral.append(aluno[:])
+    aluno.clear()
+    perg = str(input('Quer continuar? [S/N] ').strip().upper()[0])
+    while perg not in 'S' and perg not in 'N':
+        perg = str(input('Quer continuar? [S/N] ').strip().upper()[0])
+    if perg == 'N':
+        break
+print(alunosGeral)
