f1a6f9d1d288fd336be431649d9c3a76dcc42bac
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-08-02 11:25:38 -0300

    quase terminando o ex089

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -12,4 +12,17 @@ while True:
         perg = str(input('Quer continuar? [S/N] ').strip().upper()[0])
     if perg == 'N':
         break
-print(alunosGeral)
+print('-='*30)
+print(f'{'N°':<4}{'NOME':<12}{'MÉDIA'}')
+print('-'*30)
+for pos, al in enumerate(alunosGeral):
+    media = (al[1] + al[2]) / 2
+    print(f'{pos:<4}{al[0]:<12}{media:.1f}')
+while True:
+    print('-'*30)
+    mostrNotas = int(input('Mostrar notas de qual aluno? (999 interrompe): ').strip())
+    if mostrNotas == 999:
+        break
+    else:
+        if mostrNotas == pos:
+            print(f'Notas de {al[0]} são {al[1]}')
